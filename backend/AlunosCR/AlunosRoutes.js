91b3a360e8f6851681e6e4b122e0921567de631b
drey-na <drey.pyy@gmail.com>
2025-09-06 16:29:03 -0300

    primeira teste para a criação das rotas

--- backend/AlunosCR/AlunosRoutes.js
+++ backend/AlunosCR/AlunosRoutes.js
@@ -1,11 +1,10 @@
-const express = require("express");
-const router = express.Router();
-const alunosController = require("./AlunosController");
+const express = require('express');
+const router = express.router();
+const alunosController = require('/alunosController');
 
-router.get("/alunos", alunosController.listarAlunos);
-router.get("/alunos/:id", alunosController.buscarAluno);
-router.post("/alunos", alunosController.criarAluno);
-router.put("/alunos/:id", alunosController.atualizarAluno);
-router.delete("/alunos/:id", alunosController.deletarAluno);
+router.get('/alunos', alunosController.listarAlunos);
+router.post('/alunos', alunosController.criarAlunos);
+router.put('/alunos/:id', alunosController.atualizarAlunos);
+router.delete('/alunos/:id', alunosController.deletarAlunos);
 
 module.exports = router;
